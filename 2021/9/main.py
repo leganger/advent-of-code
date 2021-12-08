c00484c049e4088d71a2783272d807a693c63c78
Lars Erlend Leganger <l.e.leganger@gmail.com>
2021-12-08 12:33:16 +0100

    finish 2021-9

--- 2021/9/main.py
+++ 2021/9/main.py
@@ -50,13 +50,15 @@ puzzle.answer_a = answer
 # %%
 def basin_size(hmap,X,Y):
     if risk(hmap,X,Y)>0:
-        print("Found low point ({X},{Y})!")
+        print(f"Found low point ({X},{Y})!")
         basin_points = [(X,Y)]
         new_basin_points = []
+        scanned_list = []
         while True:
             for point in basin_points:
                 if point not in new_basin_points:
                     new_basin_points.append(point)
+                if point not in scanned_list:
                     x,y = point
                     for dx in [-1,0,1]:
                         for dy in [-1,0,1]:
@@ -72,17 +74,23 @@ def basin_size(hmap,X,Y):
                                 continue
                             if all([x+dx >= 0, x+dx < hmap.shape[1], y+dy >= 0, y+dy < hmap.shape[0]]):
                                 if 9 > hmap[y+dy][x+dx] >= hmap[y][x]:
-                                    print(f"Adding point ({x+dx},{y+dy}) to basin.")
+                                    print(f"Adding point ({x+dx},{y+dy}) with height {hmap[y+dy][x+dx]} to basin.")
                                     new_basin_points.append((x+dx,y+dy))
+                    scanned_list.append(point)
             if basin_points == new_basin_points:
-                print(f"Found low point ({X},{Y}) with basin size {len(new_basin_points)}")
-                return len(new_basin_points)
+                print(f"Found low point ({X},{Y}) with basin size {len(list(set(new_basin_points)))}")
+                return len(list(set(new_basin_points)))
             basin_points = new_basin_points
+    return 0
 
 basinmap = np.zeros(shape)
 for y,row in enumerate(dl):
     for x,col in enumerate(row):
         basinmap[y][x] = basin_size(hmap,x,y)
 
+answer = int(np.product(np.sort(basinmap.flatten())[::-1][:3]))
+
+# %%
+puzzle.answer_b = answer
 
 # %%
